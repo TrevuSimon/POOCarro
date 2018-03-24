46091d20e875924a2393fac520029b70e7731f63
Guilherme <guilherme1301703@fapi-pinhais.edu.br>
2018-03-24 01:30:23 -0300

    fabrica de carro e carro prontos

--- src/main/java/fapi/poo/listacarro/carro/Carro.java
+++ src/main/java/fapi/poo/listacarro/carro/Carro.java
@@ -3,14 +3,63 @@ package fapi.poo.listacarro.carro;
 import fapi.poo.listacarro.banco.Banco;
 import fapi.poo.listacarro.motor.Motor;
 import fapi.poo.listacarro.pneu.Pneu;
+import fapi.poo.listacarro.porta.Porta;
 import fapi.poo.listacarro.roda.Roda;
 import fapi.poo.listacarro.tanque.Tanque;
 
 public class Carro {
     protected Motor motor;
-    protected Roda roda[];
-    protected Pneu pneu[];
-    protected Banco banco[];
-    protected Tanque tanque[];
+    protected Roda roda[] = new Roda[4];
+    protected Pneu pneu[] = new Pneu[4];
+    protected Banco banco[] = new Banco[3];
+    protected Porta porta[] = new Porta[4];
+    protected Tanque tanque;
 
+    public Motor getMotor() {
+        return motor;
+    }
+
+    public void setMotor(Motor motor) {
+        this.motor = motor;
+    }
+
+    public Roda[] getRoda() {
+        return roda;
+    }
+
+    public void setRoda(Roda[] roda) {
+        this.roda = roda;
+    }
+
+    public Pneu[] getPneu() {
+        return pneu;
+    }
+
+    public void setPneu(Pneu[] pneu) {
+        this.pneu = pneu;
+    }
+
+    public Banco[] getBanco() {
+        return banco;
+    }
+
+    public void setBanco(Banco[] banco) {
+        this.banco = banco;
+    }
+
+    public Porta[] getPorta() {
+        return porta;
+    }
+
+    public void setPorta(Porta[] porta) {
+        this.porta = porta;
+    }
+
+    public Tanque getTanque() {
+        return tanque;
+    }
+
+    public void setTanque(Tanque tanque) {
+        this.tanque = tanque;
+    }
 }
--- src/main/java/fapi/poo/listacarro/carro/FabricaCarro.java
+++ src/main/java/fapi/poo/listacarro/carro/FabricaCarro.java
@@ -1,11 +1,63 @@
 package fapi.poo.listacarro.carro;
 
+import fapi.poo.listacarro.banco.Banco;
+import fapi.poo.listacarro.banco.FabricaBanco;
+import fapi.poo.listacarro.motor.FabricaMotor;
 import fapi.poo.listacarro.pneu.FabricaPneu;
+import fapi.poo.listacarro.pneu.Pneu;
+import fapi.poo.listacarro.porta.FabricaPorta;
+import fapi.poo.listacarro.porta.Porta;
+import fapi.poo.listacarro.roda.FabricaRoda;
+import fapi.poo.listacarro.roda.Roda;
+import fapi.poo.listacarro.tanque.FabricaTanque;
 
 public class FabricaCarro {
     public Carro criaCarro(){
         Carro carro = new Carro();
-        FabricaPneu pneuFabrica = new FabricaPneu();
+        FabricaBanco fabricaBanco = new FabricaBanco();
+        FabricaMotor fabricaMotor = new FabricaMotor();
+        FabricaPneu fabricaPneu = new FabricaPneu();
+        FabricaTanque fabricaTanque = new FabricaTanque();
+        FabricaPorta fabricaPorta = new FabricaPorta();
+        FabricaRoda fabricaRoda = new FabricaRoda();
+
+        //Banco
+        Banco banco[] = new Banco[3];
+        banco[0] = fabricaBanco.criaBanco();
+        banco[1] = fabricaBanco.criaBanco();
+        banco[2] = fabricaBanco.criaBanco();
+        carro.setBanco(banco);
+
+        //motor
+        carro.setMotor(fabricaMotor.criaMotor());
+
+        //pneu
+        Pneu pneu[] = new Pneu[4];
+        pneu[0] = fabricaPneu.criaPneu();
+        pneu[1] = fabricaPneu.criaPneu();
+        pneu[2] = fabricaPneu.criaPneu();
+        pneu[3] = fabricaPneu.criaPneu();
+        carro.setPneu(pneu);
+
+        //tanque
+        carro.setTanque(fabricaTanque.criaTanque());
+
+        //Porta
+        Porta porta[] = new Porta[4];
+        porta[0] = fabricaPorta.criaPorta();
+        porta[1] = fabricaPorta.criaPorta();
+        porta[2] = fabricaPorta.criaPorta();
+        porta[3] = fabricaPorta.criaPorta();
+        carro.setPorta(porta);
+
+        //Roda
+        Roda roda[] = new Roda[4];
+        roda[0] = fabricaRoda.criaRoda();
+        roda[1] = fabricaRoda.criaRoda();
+        roda[2] = fabricaRoda.criaRoda();
+        roda[3] = fabricaRoda.criaRoda();
+        carro.setRoda(roda);
+
 
         return carro;
     }
